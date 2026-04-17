20c3b47727af1d8614e3abec2d51c82ab1099d43
Christopher <soribs23@gmail.com>
2026-04-17 22:22:54 +0200

    new version

--- resource_group.tf
+++ resource_group.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source = "git::ssh://git@github.com/soribs/terraform-azure-child-of-projectA.git?ref=v1.0.0"
+  source = "git::ssh://git@github.com/soribs/terraform-azure-child-of-projectA.git?ref=v1.0.1"
 
   resource_group_name = var.resource_group_name
   project_name        = var.project_name
